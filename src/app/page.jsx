be792fc2b39b9b9e576afdd926f8572e042c1720
S1nthesis <christophervu03@gmail.com>
2024-02-19 04:52:35 -0600

    finished contact and fixed linting errors

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -25,7 +25,7 @@ const Homepage = () => {
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-            Hi, I'm Chris Vu! Welcome to my digital canvas, where innovation and creativity
+            Hi, I&#8217;m Chris Vu! Welcome to my digital canvas, where innovation and creativity
             converge. With a keen eye for aesthetics and a mastery of code, my
             portfolio showcases a diverse collection of projects that reflect my
             commitment to excellence.
