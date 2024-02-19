f4dbea6e01f216699c9ab9631aff3a6726550644
S1nthesis <christophervu03@gmail.com>
2024-02-19 01:50:18 -0600

    finished about page

--- src/components/TransitionProvider.jsx
+++ src/components/TransitionProvider.jsx
@@ -14,7 +14,6 @@ const TransitionProvider = ({ children }) => {
   const displayTitle = pathName === '/' ? 'Home' : capitalizeFirstLetter(pathName.substring(1));
 
   return (
-    // <AnimatePresence mode="wait">
     <div
       key={pathName}
       className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100"
@@ -44,7 +43,6 @@ const TransitionProvider = ({ children }) => {
       </div>
       <div className="h-[calc(100vh-6rem)]">{children}</div>
     </div>
-    // </AnimatePresence>
   );
 };
 
